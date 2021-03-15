dd24217e26b2eb1bbf28095a9d772baf24ad7c7e
icetree158 <79416180+icetree158@users.noreply.github.com>
2021-03-16 00:27:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function toReadable (number) {
-  
+    const one = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen"
+    ];
+
+    const two = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety"
+    ];
+
+    if (number == 0) return "zero";
+    if (number < 20) return one[number];
+    if (number >= 20 && number < 100) return two[parseInt(number / 10)] + (number % 10 == 0 ? one[number % 10] : (" " + one[number % 10]));
+    let numberString = number.toString();
+    if (numberString[1] == "0" && numberString[2] == "0") {
+        return one[parseInt(number / 100)] + " hundred";
+    } else {
+        return one[parseInt(number / 100)] + " hundred " + toReadable(parseInt(numberString[1] + numberString[2]));
+    }
 }
